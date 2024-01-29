96f4220d395efe74e0535e03608829cb06582815
vilasini <gulkavarsh@gmail.com>
2024-01-29 20:17:57 +0530

    some changes

--- script.js
+++ script.js
@@ -6,25 +6,36 @@ function heads(){
     var num=Math.random()*100;
     if(num<=50){
         score.wins++;
-        alert('You picked heads, computer picked heads, you won \nWins: ' + score.wins + ' Losses: ' + score.loses);
+        document.getElementById('message').innerHTML = `You picked heads, computer picked heads, you won`;
+        document.getElementById('score').innerHTML = `Wins: ${score.wins} Losses: ${score.loses}`;
+
     }else{
         score.loses++;
-        alert('You picked heads, computer picked tails, you lost \nWins: ' + score.wins + ' Losses: ' + score.loses);
+        document.getElementById('message').innerHTML = `You picked heads, computer picked tails, you lost`;
+        document.getElementById('score').innerHTML = `Wins: ${score.wins} Losses: ${score.loses}`;
+
     }
 }
 function tails(){
     var num=Math.random()*100;
     if(num>50){
         score.wins++;
-        alert('You picked tails, computer picked tails, you won \nWins: ' + score.wins + ' Losses: ' + score.loses);
+        document.getElementById('message').innerHTML = `You picked tails, computer picked tails, you won`;
+        document.getElementById('score').innerHTML = `Wins: ${score.wins} Losses: ${score.loses}`;
+
+        // alert('You picked tails, computer picked tails, you won \nWins: ' + score.wins + ' Losses: ' + score.loses);
     }else{
         score.loses++;
-        alert('You picked tails, computer picked heads, you lost \nWins: ' + score.wins + ' Losses: ' + score.loses);
+        document.getElementById('message').innerHTML = `You picked tails, computer picked heads, you lost`;
+        document.getElementById('score').innerHTML = `Wins: ${score.wins} Losses: ${score.loses}`;
+        // alert('You picked tails, computer picked heads, you lost \nWins: ' + score.wins + ' Losses: ' + score.loses);
     }
 }
 
 function reset(){
     score.wins = 0;
     score.loses = 0;
-    alert('Score has been reset, Wins: ' + score.wins + ' Losses: ' + score.loses );
+    document.getElementById('message').innerHTML = `Score has been reset`;
+    document.getElementById('score').innerHTML = `Wins: ${score.wins} Losses: ${score.loses}`;
 }
+
